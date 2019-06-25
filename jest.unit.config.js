f22390d99ccd23d9a4247c2e67594509ae383a4d
Ronen Amiel <ronen.amiel@gmail.com>
2019-06-25 20:24:04 +0300

    basic ts infra

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  testMatch: ['**/__tests__/**/*.(spec|test).js'],
   testResultsProcessor: 'jest-teamcity-reporter',
   testPathIgnorePatterns: [
     '/packages/create-yoshi-app/templates/',
     '/node_modules/',
     '/test/',
   ],
+  preset: 'ts-jest',
 };
